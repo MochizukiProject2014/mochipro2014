6895a982f39529e9b40a9004ad439d11844232a9
ne240152 <ne240152@senshu-u.jp>
2015-07-01 20:07:09 +0900

    パーサ書き換え while文

--- js/parser.js
+++ js/parser.js
@@ -153,7 +153,7 @@ parser = (function() {
                for(var k=1; k < block.length; k++){
                  a = a + block[k];
                }
-            return 'duplication_judge("int","i", null );for_js("false,i,0","i < 10","i:+:1","' + peg$computePosDetails(peg$reportedPos).line + '");' + a  + 'end_of_for();'; },
+            return 'for_js("while","' + comparison + '","","' + peg$computePosDetails(peg$reportedPos).line + '");' + a  + 'end_of_for();'; },
         peg$c84 = "break",
         peg$c85 = { type: "literal", value: "break", description: "\"break\"" },
         peg$c86 = function() {return "break_kari();" ;},
@@ -539,6 +539,7 @@ parser = (function() {
         }
 
         return "Expected " + expectedDesc + " but " + foundDesc + " found.";
+
       }
 
       var posDetails = peg$computePosDetails(pos),
