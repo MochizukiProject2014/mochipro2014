44dc52dcad46bd99d3d454a0b07ddd908b99a568
ne240152 <ne240152@senshu-u.jp>
2015-07-27 14:26:51 +0900

    例題実装　デバッグまだ

---
+++ js/samplecode4-5.js
@@ -0,0 +1,123 @@
+var ex_411 = new Array();
+ex_411.push("setIntr(\"<b>今回の挑戦状を解くには「繰り返し」が必要にゃ！ということでこのコードを見るにゃ。<br>このコードは「こんこん」と「たぬたぬ」をそれぞれ３回ずつ出力するプログラムにゃ。</b>\");;baON()");
+
+ex_411.push("baOFF();;line(3);;ANIME_sengen(\"int\",\"a\");;line(4);;line(5);;ANIME_printf(\"数字を入力してください:\");;line(6);;ANIME_scanf_ex();;setPrintf(\"3\");;ANIME_dainyu(\"a\",\"3\");;line(7);;line(11);;ANIME_printf(\"もう一度\");;line(4);;line(5);;ANIME_printf(\"数字を入力してください:\");;line(6);;ANIME_scanf_ex();;setPrintf(\"2\");;ANIME_dainyu(\"a\",\"2\");;line(7);;line(11);;ANIME_printf(\"もう一度\");;line(4);;line(5);;ANIME_printf(\"数字を入力してください:\");;line(6);;ANIME_scanf_ex();;setPrintf(\"1\");;ANIME_dainyu(\"a\",\"1\");;line(7);;line(11);;ANIME_printf(\"もう一度\");;line(4);;line(5);;ANIME_printf(\"数字を入力してください:\");;line(6);;ANIME_scanf_ex();;setPrintf(\"0\");;ANIME_dainyu(\"a\",\"0\");;line(7);;line(8);;ANIME_printf(\"0が入力されたのでプログラムを終了します。\");;line(9);;biON();;setIntr(\"<b>では、まずは4，5，6行目を見るにゃ</b>\")");
+
+ex_411.push("line_reset();;line(4);;setIntr(\"<b>上これはfor文というもので、</br>あるコードを繰り返し実行したい時につかうにゃ</b>\")")
+ex_411.push("setIntr(\"<b>『()』で囲まれた『a=1』の部分を初期化式</br>a<=3』の部分を継続条件式</br>『a++』の部分を再初期化式というにゃ。</br>『{}』で囲まれた部分を文というにゃ</br></b>\")")
+ex_411.push("setIntr(\"<b>初期化式はfor文の1行目についたときに実行されるにゃ</br>今回はaに1を代入してるにゃ</b>\")");
+ex_411.push("setIntr(\"<b>そして、継続条件式が満たされていたら文を実行するにゃ</br>ちなみに条件が満たされていることを「真」というにゃ</b>\")");
+ex_411.push("setIntr(\"<b>今回はaが3以下の間なので、今aには１が代入されているから真なので文が実行されるにゃ</br>文が実行されて「こんこん」と出力してるにゃ</b>\")");
+ex_411.push("setIntr(\"<b>そして、文の実行が終わるとfor文の1行目に戻って</br>再初期化式を実行するにゃ</br>今回はaに1を足してるにゃ</b>\")");
+ex_411.push("setIntr(\"<b>そして、継続条件式が満たされていたらもう一度文が実行されるにゃ</b>\")");
+ex_411.push("setIntr(\"<b>今回は、継続条件式がaが3以下の間なので、今aには2が代入されているからもう一度文が実行されるにゃ</br>文の実行が終わったら1行目にもどって再初期化式を実行し～をくりかえし、<br>継続条件式に当てはまる間繰り返すにゃ。<br>\")");
+ex_411.push("setIntr(\"<b>今回は3回繰り返したところで終わるようになってるにゃ</b>\")");
+ex_411.push("setIntr(\"<b>これが「繰り返し」というものにゃ</br>「繰り返し」のことを「ループ」と言ったりするので一応覚えておくといいにゃ</b>\")");
+ex_411.push("setIntr(\"<b>まとめると<br>①初期化式を実行する</br>②継続条件式が満たされていたら文を実行する。満たされていなかったらfor文を抜け出す</br>③文を実行する\")");
+ex_411.push("setIntr(\"<b>④文が終わったらfor文の1行目に戻り、再初期化式を実行する</br>⑤②～④を繰り返す</br>という感じにゃ!\")");
+ex_411.push("setIntr(\"<b>再初期化式にはインクリメント演算子がよく用いられるけど、デクリメント演算子でも、普通の数式でも可能にゃ！\")");
+ex_411.push("setIntr(\"<b>2個目の「繰り返し」ではfor文の中身が変わっているけどさっきと同じような動きをするにゃ</br>コードをよく読んでどういう動きになっているか想像するにゃ</b>\")");
+ex_411.push("setIntr(\"<b>じゃあそれを踏まえてもう一度コードの動きを見るにゃ<br>コードの動きが複雑になってくるけど落ち着いて考えれば大丈夫にゃ！\");;line_reset();;codeArrayInit();;sign=1;;baON()");
+
+ex_411.push("baOFF();;line(3);;ANIME_sengen(\"int\",\"x\");;line(4);;ANIME_sengen(\"double\",\"y\");;line(5);;ANIME_sengen(\"char\",\"z\");;line(6);;ANIME_sengen(\"int\",\"kengo\");;setIntr(\"<b>なんとなくでもわかったら、次は自分で書いてみるにゃ！</b>\");;bRestart();;return_js()");
+
+//---------------------------------------------
+
+var ex_421 = new Array();
+ex_421.push("setIntr(\"<b>これから見せるコードは、０を入力するまで数字の入力を何度も受け付けるというプログラムにゃ</br>さっきまでと違って、何回繰り返すかはわからないコードだにゃ</br>じゃあコードを動かしてみるにゃ\");;baON()");
+
+ex_421.push("baOFF();;line(3);;ANIME_sengen(\"int\",\"a\");;line(4);;line(5);;ANIME_printf(\"数字を入力してください:\");;setPrintf(\"数字を入力してください:\");;line(6);;ANIME_scanf_ex();;setPrintf(\"3\\n\");;ANIME_dainyu(\"a\",\"3\");;line(7);;line(11);;ANIME_printf(\"もう一度\");;setPrintf(\"もう一度\");;line_reset();;line(4);;line(5);;ANIME_printf(\"数字を入力してください:\");;setPrintf(\"数字を入力してください:\");;line(6);;ANIME_scanf_ex();;setPrintf(\"2\\n\");;ANIME_dainyu(\"a\",\"2\");;line(7);;line(11);;ANIME_printf(\"もう一度\");;setPrintf(\"もう一度\");;line_reset();;line(4);;line(5);;ANIME_printf(\"数字を入力してください:\");;setPrintf(\"数字を入力してください:\");;line(6);;ANIME_scanf_ex();;setPrintf(\"1\\n\");;ANIME_dainyu(\"a\",\"1\");;line(7);;line(11);;ANIME_printf(\"もう一度\");;setPrintf(\"もう一度\");;line_reset();;line(4);;line(5);;ANIME_printf(\"数字を入力してください:\");;setPrintf(\"数字を入力してください:\");;line(6);;ANIME_scanf_ex();;setPrintf(\"0\\n\");;ANIME_dainyu(\"a\",\"0\");;line(7);;line(8);;ANIME_printf(\"0が入力されたのでプログラムを終了します。\\n\");;setPrintf(\"0が入力されたのでプログラムを終了します。\\n\");;line(9);;line(13);;biON();;setIntr(\"<b>じゃあ説明していくにゃ</b>\")");
+
+ex_421.push("line_reset();;line(4);;setIntr(\"<b>まずは4～12行を見るにゃ、これが「while文」にゃ</b>\")");
+ex_421.push("setIntr(\"<b>while文はfor文と同じで「繰り返し」のコードを書くことができるにゃ</b>\")");
+ex_421.push("setIntr(\"<b>「()」で囲まれた部分が継続条件式で</br>「｛｝」で囲まれた部分が文にゃ</b>\")");
+ex_421.push("setIntr(\"<b>forと同じで、継続条件式が満たされている間文を繰り返すことのできるコードにゃ</br>forと違うのは初期化式と再初期化式がない所くらいにゃ</b>\")");
+ex_421.push("setIntr(\"<b>今回、継続条件式には1と書かれているにゃ</br>プログラミングにおいて1は「真」を表すにゃ。つまり、継続条件式が満たされていることになるにゃ</br>ちなみにプログラミングにおいて「0」は偽、つまり満たされないことになるから覚えておくにゃ</b>\")");
+ex_421.push("setIntr(\"<b>こうすることで継続条件式が常に満たされていることになり、永遠にwhile文の中を繰り返すことができるようになってるにゃ</br>\")");
+ex_421.push("setIntr(\"<b>あと、このように永遠に繰り返す構造のことを「無限ループ」と言ったりするので一応覚えておくにゃ</b>\")");
+ex_421.push("setIntr(\"<b>え？永遠に続くなら終わらないんじゃにゃいかって？</br>その通りにゃ、「無限ループ」になってしまうとコードが終わらないので</br>「繰り返し」を使う際は気を付けなければならないにゃ</b>\")");
+ex_421.push("setIntr(\"<b>じゃあなんで、今回はちゃんと終わらせることができたかというと</br>9行目にある「break」というコードのおかげにゃ</b>\")");
+ex_421.push("line(9);;setIntr(\"<b>このbreakが実行されると、ループを一つ抜け出すことができるにゃ</br>今回はwhileの中にあるので、このwhileを抜け出すことができるにゃ</b>\")");
+ex_421.push("setIntr(\"<b>breakはループの中にif文を書いて、その中に書くことが多いにゃ</br>今回は0が入力されるとif文が実行される構造になっているので、</br>0が入力されることでbreakが実行されて無限ループを抜け出して、コードを終わらせることができるにゃ\")");
+ex_421.push("setIntr(\"<b>ということで、whileの無限ループでscanfを何度も実行し、0が入力されることでif文が実行されるにゃ</br>その結果、breakが実行されて、whileを抜け出して終了しているにゃ。\")");
+ex_421.push("setIntr(\"<b>じゃあもう一度コードの動きを見るにゃ、さっきと同じように入力は3，2，1，0にゃ\");;line_reset();;codeArrayInit();;sign=1;;baON()");
+
+ex_421.push("baOFF();;line(3);;ANIME_sengen(\"int\",\"a\");;line(4);;line(5);;ANIME_printf(\"数字を入力してください:\");;setPrintf(\"数字を入力してください:\");;line(6);;ANIME_scanf_ex();;setPrintf(\"3\\n\");;ANIME_dainyu(\"a\",\"3\");;line(7);;line(11);;ANIME_printf(\"もう一度\");;setPrintf(\"もう一度\");;line_reset();;line(4);;line(5);;ANIME_printf(\"数字を入力してください:\");;setPrintf(\"数字を入力してください:\");;line(6);;ANIME_scanf_ex();;setPrintf(\"2\\n\");;ANIME_dainyu(\"a\",\"2\");;line(7);;line(11);;ANIME_printf(\"もう一度\");;setPrintf(\"もう一度\");;line_reset();;line(4);;line(5);;ANIME_printf(\"数字を入力してください:\");;setPrintf(\"数字を入力してください:\");;line(6);;ANIME_scanf_ex();;setPrintf(\"1\\n\");;ANIME_dainyu(\"a\",\"1\");;line(7);;line(11);;ANIME_printf(\"もう一度\");;setPrintf(\"もう一度\");;line_reset();;line(4);;line(5);;ANIME_printf(\"数字を入力してください:\");;setPrintf(\"数字を入力してください:\");;line(6);;ANIME_scanf_ex();;setPrintf(\"0\\n\");;ANIME_dainyu(\"a\",\"0\");;line(7);;line(8);;ANIME_printf(\"0が入力されたのでプログラムを終了します。\\n\");;setPrintf(\"0が入力されたのでプログラムを終了します。\\n\");;line(9);;line(13);;biON();;setIntr(\"<b>わかったかにゃ？\")");
+
+ex_421.push("line_reset();;setIntr(\"<b>ちなみにfor文でもwhile文でも同じ動きのコードを書くことができるにゃ</br>なんで二つの書き方があるかというと、それぞれ得意分野があるにゃ</b>\")");
+ex_421.push("setIntr(\"<b>for文は今までやってきたみたいな、ある程度何回繰り返されるかわかっている場合が得意で、</br>while文は、今回みたいな何回繰り返されるかわからないときが得意にゃ</br>どっちで書いても問題ないけどこういった特性を覚えておくと便利にゃ</b>\")");
+ex_421.push("setIntr(\"<b>じゃあ、whileの使い方を問題を解いて練習していくにゃ！\");;bRestart();;return_js()");
+
+//---------------------------------------------
+
+var ex_431 = new Array();
+ex_431.push("setIntr(\"<b>これから見せるコードは、「＊」を4個ずつ書いたものを3行作るコードにゃ</br>まずは動きを見るにゃ、複雑な動きをするからよく見ておくにゃ\");;baON()");
+
+ex_431.push("baOFF();;line(3);;ANIME_sengen(\"int\",\"i\");;ANIME_sengen(\"int\",\"n\");;line(4);;ANIME_dainyu(\"n\",\"0\");;line(5);;ANIME_dainyu(\"i\",\"0\");;line(6);;ANIME_printf(\"*\");;setPrintf(\"*\");;line(5);;ANIME_enzan_dainyu(\"i\",\"i+1\",\"1\");;line(6);;ANIME_printf(\"*\");;setPrintf(\"*\");;line(5);;ANIME_enzan_dainyu(\"i\",\"i+1\",\"2\");;line(6);;ANIME_printf(\"*\");;setPrintf(\"*\");;line(5);;ANIME_enzan_dainyu(\"i\",\"i+1\",\"3\");;line(6);;ANIME_printf(\"*\");;setPrintf(\"*\");;line(5);;ANIME_enzan_dainyu(\"i\",\"i+1\",\"4\");;line(8);;ANIME_printf(\"\\n\");;setPrintf(\"\\n\");;line(4);;ANIME_enzan_dainyu(\"n\",\"n+1\",\"1\");;line(5);;ANIME_dainyu(\"i\",\"0\");;line(6);;ANIME_printf(\"*\");;setPrintf(\"*\");;line(5);;ANIME_enzan_dainyu(\"i\",\"i+1\",\"1\");;line(6);;ANIME_printf(\"*\");;setPrintf(\"*\");;line(5);;ANIME_enzan_dainyu(\"i\",\"i+1\",\"2\");;line(6);;ANIME_printf(\"*\");;setPrintf(\"*\");;line(5);;ANIME_enzan_dainyu(\"i\",\"i+1\",\"3\");;line(6);;ANIME_printf(\"*\");;setPrintf(\"*\");;line(5);;ANIME_enzan_dainyu(\"i\",\"i+1\",\"4\");;line(8);;ANIME_printf(\"\\n\");;setPrintf(\"\\n\");;line(4);;ANIME_enzan_dainyu(\"n\",\"n+1\",\"2\");;line(5);;ANIME_dainyu(\"i\",\"0\");;line(6);;ANIME_printf(\"*\");;setPrintf(\"*\");;line(5);;ANIME_enzan_dainyu(\"i\",\"i+1\",\"1\");;line(6);;ANIME_printf(\"*\");;setPrintf(\"*\");;line(5);;ANIME_enzan_dainyu(\"i\",\"i+1\",\"2\");;line(6);;ANIME_printf(\"*\");;setPrintf(\"*\");;line(5);;ANIME_enzan_dainyu(\"i\",\"i+1\",\"3\");;line(6);;ANIME_printf(\"*\");;setPrintf(\"*\");;line(5);;ANIME_enzan_dainyu(\"i\",\"i+1\",\"4\");;line(8);;ANIME_printf(\"\\n\");;setPrintf(\"\\n\");; line(4);;ANIME_enzan_dainyu(\"n\",\"n+1\",\"3\");;line(10);;setIntr(\"<b>じゃあ説明していくにゃ</b>\");;biON()");
+
+ex_431.push("line_reset();;line(4);;setIntr(\"<b>まずは、4，5行目を見るにゃ</br>for文の中にfor文が入っているのがわかるかにゃ？\")");
+ex_431.push("setIntr(\"<b>今回みたいに、「*」を出力を4回繰り返し、それを3行分だから3回繰り返す、みたいに繰り返すものを繰り返したい時に、このようなfor文の中にfor文を入れるコードを書くにゃ</br>このfor文の中にfor文を入れることを「二重ループ」っていうにゃ</b>\")");
+ex_431.push("setIntr(\"<b>まず、一つ目のfor文で5回繰り返すようにしてるにゃ、</br>これが5行にするためのループだにゃ</b>\")");
+ex_431.push("line(5);;setIntr(\"<b>そしてその中に「*」を出力を10回繰り返すfor文を書いてるにゃ。</br>これで4個「*」が出力できるにゃ</b>\")");
+ex_431.push("line(8);;setIntr(\"<b>そして4個「*」を出力したら内側にあるループを抜けて、ここで一回改行してるにゃ</b>\")");
+ex_431.push("setIntr(\"<b>\\nは改行という意味だにゃ</br>改行したら一つ目ののループの「｝」につくので、上に戻ってまた繰り返すにゃ</b>\")");
+ex_431.push("setIntr(\"<b>少し複雑な動きだけどついてこれてるかにゃ?</br>今の話を踏まえてもう一度動きを見てみるにゃ</b>\");;line_reset();;codeArrayInit();;sign=1;;baON()");
+
+ex_431.push("baOFF();;biOFF();;line(3);;ANIME_sengen(\"int\",\"i\");;ANIME_sengen(\"int\",\"n\");;line(4);;ANIME_dainyu(\"n\",\"0\");;line(5);;ANIME_dainyu(\"i\",\"0\");;line(6);;ANIME_printf(\"*\");;setPrintf(\"*\");;line(5);;ANIME_enzan_dainyu(\"i\",\"i+1\",\"1\");;line(6);;ANIME_printf(\"*\");;setPrintf(\"*\");;line(5);;ANIME_enzan_dainyu(\"i\",\"i+1\",\"2\");;line(6);;ANIME_printf(\"*\");;setPrintf(\"*\");;line(5);;ANIME_enzan_dainyu(\"i\",\"i+1\",\"3\");;line(6);;ANIME_printf(\"*\");;setPrintf(\"*\");;line(5);;ANIME_enzan_dainyu(\"i\",\"i+1\",\"4\");;line(8);;ANIME_printf(\"\\n\");;setPrintf(\"\\n\");;line(4);;ANIME_enzan_dainyu(\"n\",\"n+1\",\"1\");;line(5);;ANIME_dainyu(\"i\",\"0\");;line(6);;ANIME_printf(\"*\");;setPrintf(\"*\");;line(5);;ANIME_enzan_dainyu(\"i\",\"i+1\",\"1\");;line(6);;ANIME_printf(\"*\");;setPrintf(\"*\");;line(5);;ANIME_enzan_dainyu(\"i\",\"i+1\",\"2\");;line(6);;ANIME_printf(\"*\");;setPrintf(\"*\");;line(5);;ANIME_enzan_dainyu(\"i\",\"i+1\",\"3\");;line(6);;ANIME_printf(\"*\");;setPrintf(\"*\");;line(5);;ANIME_enzan_dainyu(\"i\",\"i+1\",\"4\");;line(8);;ANIME_printf(\"\\n\");;setPrintf(\"\\n\");;line(4);;ANIME_enzan_dainyu(\"n\",\"n+1\",\"2\");;line(5);;ANIME_dainyu(\"i\",\"0\");;line(6);;ANIME_printf(\"*\");;setPrintf(\"*\");;line(5);;ANIME_enzan_dainyu(\"i\",\"i+1\",\"1\");;line(6);;ANIME_printf(\"*\");;setPrintf(\"*\");;line(5);;ANIME_enzan_dainyu(\"i\",\"i+1\",\"2\");;line(6);;ANIME_printf(\"*\");;setPrintf(\"*\");;line(5);;ANIME_enzan_dainyu(\"i\",\"i+1\",\"3\");;line(6);;ANIME_printf(\"*\");;setPrintf(\"*\");;line(5);;ANIME_enzan_dainyu(\"i\",\"i+1\",\"4\");;line(8);;ANIME_printf(\"\\n\");;setPrintf(\"\\n\");; line(4);;ANIME_enzan_dainyu(\"n\",\"n+1\",\"3\");;line(10);;setIntr(\"<b>わかったかにゃ？ちょっと難しいけど踏ん張りどころにゃ！</br>じゃあ問題を解いて練習するにゃ!</b>\");;bRestart();;return_js()");
+
+//---------------------------------------------
+
+var ex_511 = new Array();
+ex_511.push("setIntr(\"<b>今回は「配列」というものを学んでいくにゃ</br>まずはコードの動きを見るにゃ</b>\");;baON()");
+
+ex_511.push("baOFF();;line(3);;ANIME_array_sengen_dainyu(\"int\",\"a\",3,[\"2\",\"6\",\"3\"],[\"2\",\"6\",\"3\"]);;line(4);;ANIME_sengen(\"int\",\"i\");;line(5);;ANIME_dainyu(\"i\",\"0\");;line(6);;ANIME_printf([\"a[\",\"%d\",\"] = \",\"%d\",\"\\n\"],[\"i\",\"a[0]\"]);;setPrintf(\"a[0] = 2\\n\");;line(5);;ANIME_enzan_dainyu(\"i\",\"i+1\",\"1\");;line(6);;ANIME_printf([\"a[\",\"%d\",\"] = \",\"%d\",\"\\n\"],[\"i\",\"a[1]\"]);;setPrintf(\"a[1] = 6\\n\");;line(5);;ANIME_enzan_dainyu(\"i\",\"i+1\",\"2\");;line(6);;ANIME_printf([\"a[\",\"%d\",\"] = \",\"%d\",\"\\n\"],[\"i\",\"a[2]\"]);;setPrintf(\"a[2] = 3\\n\");;line(5);;ANIME_enzan_dainyu(\"i\",\"i+1\",\"3\");;line(8);;biON();;setIntr(\"<b>ということで説明していくにゃ</b>\")");
+
+ex_511.push("line_reset();;setIntr(\"<b>電車みたいなものが出てきたのは気づいたかにゃ？</br>この電車が「配列」というもので、車両一つ一つが変数のようなものにゃ</b>\")")
+ex_511.push("line(3);;setIntr(\"<b>コードの3行目を見るにゃ。このコードで配列を宣言および初期化をしてるにゃ</br>配列というのは、今までいろいろと変数を宣言してきたと思うけど、その一種だにゃ</b>\")")
+ex_511.push("setIntr(\"<b>int aなので、とりあえず今まで見てきた部分はそのままで、</br>int型でaという名前の変数を宣言しているのはわかるかにゃ?</b>\")");
+ex_511.push("setIntr(\"<b>そして名前の後ろに[3]というのがついてるにゃ。</br>配列というのは、同じデータ型の変数をいくつかつなげたようなもので、</br>この[3]というのでつなげる数を決めてるにゃ</b>\")");
+ex_511.push("setIntr(\"<b>今回は[3]なので3つつなげてるにゃ</br>つまり、int a[3]で、「aという名前の大きさが3の配列を宣言」しているということだにゃ</b>\")");
+ex_511.push("setIntr(\"<b>そして、その後ろのコードで配列を初期化してるにゃ</br>この、配列内の各数字のことを「配列の要素」というにゃ</b>\")");
+ex_511.push("setIntr(\"<b>配列を初期化する際はこのように｛｝に入れたい数字を順番に書いていくにゃ</b>\")");
+ex_511.push("setIntr(\"<b>じゃあもう一度動きを見てみるにゃ</b>\");;line_reset();;codeArrayInit();;sign=1;;baON()");
+
+ex_511.push("baOFF();;line(3);;ANIME_array_sengen_dainyu(\"int\",\"a\",3,[\"2\",\"6\",\"3\"],[\"2\",\"6\",\"3\"]);;line(4);;ANIME_sengen(\"int\",\"i\");;line(5);;ANIME_dainyu(\"i\",\"0\");;line(6);;ANIME_printf([\"a[\",\"%d\",\"] = \",\"%d\",\"\\n\"],[\"i\",\"a[0]\"]);;setPrintf(\"a[0] = 2\\n\");;line(5);;ANIME_enzan_dainyu(\"i\",\"i+1\",\"1\");;line(6);;ANIME_printf([\"a[\",\"%d\",\"] = \",\"%d\",\"\\n\"],[\"i\",\"a[1]\"]);;setPrintf(\"a[1] = 6\\n\");;line(5);;ANIME_enzan_dainyu(\"i\",\"i+1\",\"2\");;line(6);;ANIME_printf([\"a[\",\"%d\",\"] = \",\"%d\",\"\\n\"],[\"i\",\"a[2]\"]);;setPrintf(\"a[2] = 3\\n\");;line(5);;ANIME_enzan_dainyu(\"i\",\"i+1\",\"3\");;line(8);;biON();;setIntr(\"<b>どうかにゃ？</br>よくわからなかったらもう一度動きを見るにゃ。</b>\")");
+
+ex_511.push("setIntr(\"<b>いつもならこの後は問題だけど、今回はもう一度別の例題を見てほしいので、次に進んでまた別の説明を聞いてほしいにゃ</br>\");;bRestart();;return_js()");
+
+//---------------------------------------------
+
+var ex_512 = new Array();
+ex_512.push("setIntr(\"<b>ということで、今回は続けて説明だにゃ！</br>難しいところなので、我慢して聞いてほしいにゃ</b>\");;baON()");
+
+ex_512.push("baOFF();;line(3);;ANIME_array_sengen(\"int\",\"a\",\"5\");;line(4);;ANIME_sengen(\"int\",\"i\");;line(5);;ANIME_dainyu(\"i\",\"0\");;line(6);;ANIME_scanf_ex();;setPrintf(\"0\\n\");;ANIME_array_dainyu(\"a[0]\",\"0\");;line(5);;ANIME_enzan_dainyu(\"i\",\"i+1\",\"1\");;line(6);;ANIME_scanf_ex();;setPrintf(\"1\\n\");;ANIME_array_dainyu(\"a[1]\",\"1\");;line(5);;ANIME_enzan_dainyu(\"i\",\"i+1\",\"2\");;line(6);;ANIME_scanf_ex();;setPrintf(\"2\\n\");;ANIME_array_dainyu(\"a[2]\",\"2\");;line(5);;ANIME_enzan_dainyu(\"i\",\"i+1\",\"3\");;line(8);;ANIME_dainyu(\"i\",\"0\");;line(9);;ANIME_printf([\"a[\",\"%d\",\"] = \",\"%d\",\"\\n\"],[\"i\",\"a[0]\"]);;setPrintf(\"a[0] = 0\\n\");;line(8);;ANIME_enzan_dainyu(\"i\",\"i+1\",\"1\");;line(9);;ANIME_printf([\"a[\",\"%d\",\"] = \",\"%d\",\"\\n\"],[\"i\",\"a[1]\"]);;setPrintf(\"a[1] = 1\\n\");;line(8);;ANIME_enzan_dainyu(\"i\",\"i+1\",\"2\");;line(9);;ANIME_printf([\"a[\",\"%d\",\"] = \",\"%d\",\"\\n\"],[\"i\",\"a[2]\"]);;setPrintf(\"a[2] = 2\\n\");;line(8);;ANIME_enzan_dainyu(\"i\",\"i+1\",\"3\");;line(11);;biON();;setIntr(\"<b>じゃあ説明するにゃ</b>\")");
+
+ex_512.push("line_reset();;setIntr(\"<b>さっきは配列の宣言および初期化の仕方について教えたにゃ</br>今回は、配列の「扱い方」について説明するにゃ</b>\")")
+ex_512.push("line(3);;setIntr(\"<b>まずは3行目だにゃ</br>ここで、配列を宣言してるのはわかるかにゃ？</br>今回は「aという名前の大きさが5の配列を宣言」しているにゃ</b>\")")
+ex_512.push("setIntr(\"<b>今回は宣言のみで初期化は行ってないにゃ</br>なので、あとから代入していくにゃ</b>\")");
+ex_512.push("setIntr(\"<b>5~7行目を見るにゃ</br>ここで、scanfを使ってキーボードから配列に代入してるにゃ</br>scanfの中のa[i]というのが分かるかにゃ?これで配列の一か所だけを呼ぶことができるにゃ</b>\")");
+ex_512.push("setIntr(\"<b>ここで注意してほしいことがあるにゃ</br>配列の先頭は「1番目」じゃなくて「０番目」ということにゃ</b>\")");
+ex_512.push("setIntr(\"<b>つまり、配列の一番最初に1を代入したい時は、a[0]=1　みたいに書くにゃ</br>ちなみに配列に好きな数字を代入していく場合、for文を使うと便利にゃ</b>\")");
+ex_512.push("setIntr(\"<b>そして8～10行目を見るにゃ</br>さっきと同じように配列の中身を1個だけ呼ぶ場合はa[0]とかa[1]で呼ぶにゃ</br>で、printfで呼ぶ場合は今まで通り、int型だから％dで呼ぶにゃ</b>\")");
+ex_512.push("setIntr(\"<b>じゃあここまでを踏まえてもう一度動きを見るにゃ</b>\");;line_reset();;codeArrayInit();;sign=1;;baON()");
+
+ex_512.push("baOFF();;line(3);;ANIME_array_sengen(\"int\",\"a\",\"5\");;line(4);;ANIME_sengen(\"int\",\"i\");;line(5);;ANIME_dainyu(\"i\",\"0\");;line(6);;ANIME_scanf_ex();;setPrintf(\"0\\n\");;ANIME_array_dainyu(\"a[0]\",\"0\");;line(5);;ANIME_enzan_dainyu(\"i\",\"i+1\",\"1\");;line(6);;ANIME_scanf_ex();;setPrintf(\"1\\n\");;ANIME_array_dainyu(\"a[1]\",\"1\");;line(5);;ANIME_enzan_dainyu(\"i\",\"i+1\",\"2\");;line(6);;ANIME_scanf_ex();;setPrintf(\"2\\n\");;ANIME_array_dainyu(\"a[2]\",\"2\");;line(5);;ANIME_enzan_dainyu(\"i\",\"i+1\",\"3\");;line(8);;ANIME_dainyu(\"i\",\"0\");;line(9);;ANIME_printf([\"a[\",\"%d\",\"] = \",\"%d\",\"\\n\"],[\"i\",\"a[0]\"]);;setPrintf(\"a[0] = 0\\n\");;line(8);;ANIME_enzan_dainyu(\"i\",\"i+1\",\"1\");;line(9);;ANIME_printf([\"a[\",\"%d\",\"] = \",\"%d\",\"\\n\"],[\"i\",\"a[1]\"]);;setPrintf(\"a[1] = 1\\n\");;line(8);;ANIME_enzan_dainyu(\"i\",\"i+1\",\"2\");;line(9);;ANIME_printf([\"a[\",\"%d\",\"] = \",\"%d\",\"\\n\"],[\"i\",\"a[2]\"]);;setPrintf(\"a[2] = 2\\n\");;line(8);;ANIME_enzan_dainyu(\"i\",\"i+1\",\"3\");;line(11);;setIntr(\"<b>どうかにゃ？宣言や扱い方はわかったかにゃ？</br>じゃあ問題に行くにゃ！。</b>\");;bRestart();;return_js()");
+
+//---------------------------------------------
+
+var ex_521 = new Array();
+ex_521.push("setIntr(\"<b>つぎは「二次元配列」というものを学んでいくにゃ</br>配列の進化系って感じかにゃ</br>まずは動きを見るにゃ</b>\");;baON()");
+
+ex_521.push("baOFF();;line(3);;ANIME_sengen(\"int\",\"a\");;setIntr(\"<b>じゃあ説明していくにゃ</b>\")");
+
+ex_521.push("line_reset();;setIntr(\"<b>電車の形が少し変わってるのには気づいたかにゃ？</br>これが二次元配列だにゃ</b>\")");
+ex_521.push("setIntr(\"<b>まず言葉で説明すると、</br>二次元配列というのは、配列を要素として持ってる配列だにゃ</br>なんとなくわかるかにゃ？</b>\")");
+ex_521.push("line(3);;setIntr(\"<b>じゃあコードの3行目を見るにゃ</br>この部分で「dataという名前の二次元配列を宣言」してるにゃ</br>さっきまでの配列の宣言と違うのは名前の後の大きさの指定のところにゃ</b>\")");
+ex_521.push("setIntr(\"<b>[3][3]なので、この場合は大きさが[3]の配列を要素として「3つ」持つ配列だにゃ</br>[2][3]だったら、大きさが[3]の配列を要素として「2つ」持つ配列という感じにゃ</b>\")");
+ex_521.push("setIntr(\"<b>二次元配列を初期化する場合は{{},{},{}}のような感じで初期化するにゃ</br>よく見て書き方も覚えておくにゃ</br>間違いやすいので、辞書もちゃんと見るといいにゃ</b>\")");
+ex_521.push("setIntr(\"<b>次は6行目を見るにゃ</br>二次元配列の扱い方はa[][]で呼び出すにゃ</br>今までの配列と同じ様に、一番最初は０なので注意するにゃ</b>\")");
+ex_521.push("setIntr(\"<b>だから、「二次元配列内の、最初の配列の、最初の要素を呼び出す」場合はa[0][0]で呼び出すにゃ</b>\")");
+ex_521.push("setIntr(\"<b>これが二次元配列だにゃ</br>呼び出したりするときに混乱しやすいので落ち着いて判断するにゃ</b>\")");
+ex_521.push("setIntr(\"<b>じゃあもう一度コードの動きを見るにゃ\");;line_reset();;codeArrayInit();;sign=1;;baON()");
+
+ex_521.push("baOFF();;line(3);;ANIME_sengen(\"int\",\"a\");;setIntr(\"<b>じゃあいよいよ問題にゃ！</br>がんばるにゃ！</b>\");;bRestart();;return_js()");
